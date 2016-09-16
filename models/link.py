80f66a502db7d9ebe34ef4500dd80872fc22f394
Marek Newton <marek.s.newton@gmail.com>
2016-09-15 21:54:55 -0600

    worked on adding links

--- models/link.py
+++ models/link.py
@@ -1,36 +1,42 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String, Column
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from werkzeug import generate_password_hash, check_password_hash
+from sqlalchemy.orm import sessionmaker
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+Base = declarative_base()
 
-class Link(db.Model):
-    id = db.Column('link_id', db.Integer, primary_key=True)
-    link = db.Column(String)
-    description = db.Column(String)
-    views = db.Column(Integer)
-    user = db.Column(db.String(100))
 
-    def __init__(self, link, description, views):
+engine = create_engine('sqlite:///users.db', echo=True)
+Db_session = sessionmaker(bind=engine)
+db_session = Db_session()
+
+
+class Link(Base):
+    __tablename__="Link"
+    id = Column('link_id', Integer, primary_key=True)
+    link = Column(String)
+    description = Column(String)
+    views = Column(Integer)
+    owner = Column(String(100))
+
+    def __init__(self, link, description, views, owner):
         self.link = link
         self.description = description
         self.views = views
+        self.owner = owner
 
 
-class User(db.Model):
-    id = db.Column('user_id', db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(50))
+class User(Base):
+    __tablename__="User"
+    id = Column('user_id', Integer, primary_key=True)
+    username = Column(String(100), unique=True)
+    password = Column(String(50))
 
-    def __init__(self, username, password, user):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.user = user
 
     def hash_password(self, password):
         self.password = generate_password_hash(password)
@@ -39,4 +45,6 @@ class User(db.Model):
         return check_password_hash(self.password, password)
 
 
-db.create_all()
+Base.metadata.create_all(engine)
+
+
--- routes/main.py
+++ routes/main.py
@@ -1,18 +1,18 @@
 from flask import Blueprint, render_template, request, flash, session, redirect, url_for
-from models.link import db
-from models.link import Link
-from models.link import User
+from models.link import Link, User, db_session
+from sqlalchemy import *
+from sqlalchemy.orm import *
 main = Blueprint('main', __name__)
 
 
 @main.route("/")
 def home():
-    logged_in = None
-    username = None
     if session['logged_in']:
         username = session['username']
-        logged_in = True
-    return render_template('home.html', username=username, logged_in=logged_in)
+        logged_in = session['logged_in']
+    all_links = db_session.query(Link).all()
+    print all_links
+    return render_template('home.html', username=username, logged_in=logged_in, all_links=all_links)
 
 
 @main.route("/signup", methods=['GET', 'POST'])
@@ -20,33 +20,38 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        exists = db.session.query(db.session.query(User).filter_by(username=username).exists()).scalar()
+        retype_password = request.form['retype_password']
+        exists = db_session.query(db_session.query(User).filter_by(username=username).exists()).scalar()
         if exists:
             flash("that username was already used")
             return redirect(url_for("main.signup"))
         else:
-            user = User(username, password)
-            db.session.add(user)
-            db.session.commit()
-            flash('your account was successfully created')
-            return redirect(url_for('main.home'))
+            if password == retype_password:
+                user = User(username, password)
+                db_session.add(user)
+                db_session.commit()
+                flash('your account was successfully created')
+                return redirect(url_for('main.home'))
     return render_template('signup.html')
 
 
 @main.route("/login", methods=['GET', 'POST'])
 def login():
     if session['logged_in']:
-        return redirect(url_for('main.profile'))
+        username = session['username']
+        return redirect(url_for('main.profile', username=username))
+    else:
+        render_template('login.html')
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        query = User.query.filter(User.username == username, User.password == password)
+        query = db_session.query(User).filter(User.username == username, User.password == password)
         result = query.first()
         if result:
             session['logged_in'] = True
             session['username'] = username
             flash("your logged in")
-            return redirect(url_for('main.profile'))
+            return redirect(url_for('main.profile', username=session['username']))
         else:
             flash("your usename of password is not right")
             return redirect(url_for('main.login'))
@@ -55,7 +60,8 @@ def login():
 
 @main.route("/logout")
 def logout():
-    session.pop('logged_in', None)
+    session['logged_in'] = False
+    session['username'] = None
     return redirect(url_for('main.home'))
 
 
@@ -66,6 +72,7 @@ def logged_in():
         return False
 
 
+
 @main.route("/profile/<username>", methods=['GET', 'POST'])
 def profile(username):
     if not logged_in():
@@ -73,16 +80,38 @@ def profile(username):
     else:
         if request.method == 'POST':
             link = request.form['link']
-            description = request.form['description']
-            exits = db.session.query(db.session.query(Link).filter_by(link=link).exists()).scalar()
-            if not exits:
+            link_exists = db_session.query(exists().where(Link.link == link)).scalar()
+            user_number = db_session.query(Link).filter(Link.owner == session['username']).count()
+            print user_number
+            print "panda"
+            if not link_exists and user_number < 5:
+                link = request.form['link']
+                description = request.form['description']
                 username = session['username']
-                link = Link(link, description, username, 0)
-                db.session.add(link)
-                db.session.commit()
-                redirect(url_for('main.profile', username = session['username']))
+                link = Link(link, description, 0, username)
+                db_session.add(link)
+                db_session.commit()
+                redirect(url_for('main.profile', username=session['username']))
             else:
-                flash("someone already posted that link")
-                return redirect(url_for('main.profile', username = session['username']))
-        users_links = Link.query.filter(Link.user == session['username'])
-        return render_template('profile.html', users_links = users_links, username=session['username'])
+                if user_number > 5:
+                    flash("you are at your maximum 5 links you need to delete one to add another")
+                elif link_exists:
+                    flash("someone already posted that link")
+                return redirect(url_for('main.profile', username=session['username']))
+        url_username = request.args.get('username')
+        #print url_username
+        #if url_username == session['username']:
+        users_links = db_session.query(Link).all()
+        print users_links
+        return render_template('profile.html', links=users_links, username=session['username'])
+       # else:
+            #return redirect(url_for('main.home'))
+
+@main.errorhandler(404)
+def page_not_found():
+    return render_template('404.html')
+
+
+@main.errorhandler(500)
+def server_err():
+    return render_template('500.html')
